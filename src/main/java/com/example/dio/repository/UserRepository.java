5269ad04d2c06b0090e5dfd363d4ac65f500e4ea
VishalKholakiya369 <vishalkholakiya369@gmail.com>
2025-02-27 14:31:31 +0530

    Repository Created & Updated

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.dio.repository;
 
-public class userRepository {
+import com.example.dio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+
 }
