491a07dd01e081e6e82c67f60141ddaffc596860
VishalKholakiya369 <vishalkholakiya369@gmail.com>
2025-03-04 12:52:51 +0530

    Applide validations for fields

--- src/main/java/com/example/dio/dto/response/UserResponse.java
+++ src/main/java/com/example/dio/dto/response/UserResponse.java
@@ -1,14 +1,11 @@
 package com.example.dio.dto.response;
 
 import com.example.dio.enums.UserRole;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 @Getter
 @Setter
-@Builder
 public class UserResponse {
     private long userId;
     private String username;
