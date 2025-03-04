f8a4d488ce2288c4c33853b9631121ec1cd1e976
VishalKholakiya369 <vishalkholakiya369@gmail.com>
2025-03-04 12:56:11 +0530

    Created FieldErrorResponse Exception to know user what perticular filed give error and what is that error

--- src/main/java/com/example/dio/util/FieldErrorResponse.java
+++ src/main/java/com/example/dio/util/FieldErrorResponse.java
@@ -1,4 +1,35 @@
 package com.example.dio.util;
 
-public class FieldErrorResponse {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@SuperBuilder
+public class FieldErrorResponse extends  SimpleErrorResponse{
+
+    private List<CustomFieldError> errors;
+
+    public static CustomFieldError createFiledError(String message, Object rejectedValue,String field){
+        CustomFieldError error = new CustomFieldError();
+        error.message = message;
+        error.rejectedValue = rejectedValue;
+        error.field = field;
+
+        return error;
+
+    }
+
+
+
+    @Getter
+    public static class CustomFieldError{
+      private String message;
+      private Object rejectedValue;
+      private String field;
+    }
+
 }
