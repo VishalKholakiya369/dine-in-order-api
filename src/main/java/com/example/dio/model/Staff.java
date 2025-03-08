72b65629f2bbf3664d1ea6e463d1273d5a56f399
VishalKholakiya369 <vishalkholakiya369@gmail.com>
2025-03-08 16:28:39 +0530

    Created tableEntity and added createTable feature.

--- src/main/java/com/example/dio/model/Staff.java
+++ src/main/java/com/example/dio/model/Staff.java
@@ -1,11 +1,17 @@
 package com.example.dio.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name="staffs")
 public class Staff extends User{
 
+    @ManyToMany
+    private List<RestaurantTable> tables;
 
 }
